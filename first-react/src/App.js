e7ac546f8876630171fcca08ac3a7e9ebbbd8220
VenkataSindhuPokala <sindhu@divami.com>
2023-02-22 15:18:36 +0530

    counter updated

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import Counter from './Counter';
-import './App.css';
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  function incrementCount() {
-    setCount(count + 1);
-  }
+import "./App.css";
+import { useState } from "react";
+export default function App() {
+  let [count, setCount] = useState(0);
+  
   return (
-    <div>
-    <Counter count={count} onIncrement={incrementCount} />
-  </div>
-
+    <>
+      <center>
+        <h1>Counter App</h1>
+        <h1>updated</h1>
+        <div class="b_color">
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => setCount((count = 0))}>reset</button>
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <div>{count}</div>
+        </div>
+      </center>
+    </>
   );
 }
-export default App;
-
-
 
